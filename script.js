5b966471721028f4d21385802e74c7f830abd8bd
Jacob Hoss <jacobhenryhoss2@gmail.com>
2020-07-07 18:32:56 -0400

    improved script file. made it so the event listener doesn't listen to clicks until the start quiz button is pressed. changed styling on the index.html. added start quiz button. added the rest of the questions.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var anResult = document.getElementById("answer-result");
 var score = document.getElementById("score");
 
 var points = 0;
+
 var secondsLeft = 60;
 
 function setTime() {
@@ -20,19 +21,17 @@ function setTime() {
     }
   }, 1000);
 }
-
+ 
 function changePage() {
   window.location.href = "./ending.html";
-  var points = points;
 }
 
 function displayResult(isCorrect) {
     if (isCorrect) {
         points = points + 10;
-        anResult.textContent = "Right! +10 Points! :)"
+        anResult.textContent = "Right!"
     } else {
-        points = points - 10;
-        anResult.textContent = "Wrong! -10 Points! :("
+        anResult.textContent = "Wrong!"
     }
     score.textContent = points
     enableButtons(false); // Prevent that user keeps clicking buttons
@@ -44,18 +43,17 @@ function enableButtons(on) {
     }
 }
 
-// setTime();
-
 function startQuiz() {
     displayQuestion();
     setTime();
+    listenFunction();
 }
 
-var questions = [{ 
-    question: "What is the best selling car of all time?", 
+var questions = [{
+    question: "What is the best selling car of all time?",
     answers: ["Toyota Camry", "Ford F-150", "Honda Civic", "Toyota Corolla"],
     correct: 3
-}, { 
+}, {
     question: "First introduced in 1974, which iconic hatchback still remains popular today?",
     answers: ["Ford Mustang", "Volkswagen Golf", "Toyota Celica", "Fiat 500"],
     correct: 1
@@ -63,14 +61,39 @@ var questions = [{
     question: "What is the world's fastest production car as of July 2020?",
     answers: ["Koenigsegg Agera RS", "Hennessy Venom GT", "Buggatti Chiron Super Sport", "Lamborghini Aventador"],
     correct: 2
-}, { 
+}, {
     question: "What 70s car is known for its suicide doors and convertible top?",
     answers: ["Lincoln Continental", "Buick Rivera", "Ford Thunderbird", "Oldsmobile Toronado"],
     correct: 0
+}, {
+    question: "What country was known for putting side mirrors above the front fenders?",
+    answers: ["Germany", "United States", "United Kingdom", "Japan"],
+    correct: 3
+}, {
+    question: "Which classic Mercedes model was known for its iconic wing doors?",
+    answers: ["Mercedes-Benz 300 SL", "Mercedes-Benz S600", "Mercedes-Benz W460 G-Class", "Mercedes-Benz W180"],
+    correct: 0
+}, {
+    question: "Which of these classic British sports cars boasts a V12 engine?",
+    answers: ["1956 Austin-Healey 3000", "1963 Jaguar E-Type", "1962 MGA Roadster", "1963 Aston Martin DB5"],
+    correct: 1
+}, {
+    question: "Which American model was produced from 1971 to 1977 and sold 2 million units?",
+    answers: ["Chevrolet Impala", "Pontiac Firebird", "Chevrolet Vega", "Ford Mustang"],
+    correct: 2
+}, {
+    question: "Still a popular American car today, this model was first introduced in 1976.",
+    answers: ["Chevrolet Spark", "Ford Fiesta", "Buick Verano", "GMC Sierra"],
+    correct: 1
+}, {
+    question: "Two American rivals both produced utility coupes in the 1970s. One was the Chevrolet El Camino, what was the other?",
+    answers: ["Ford Mustang", "Ford Thunderbird", "Pontiac Firebird", "Ford Ranchero"],
+    correct: 3
 }];
 
 var currentQuestion = 0;
-// displayQuestion();
+
+function listenFunction() {
 
 document.addEventListener("click", function (e) { // One click handler for all four buttons
     let buttonNum = answerButtons.indexOf(e.target);
@@ -78,10 +101,15 @@ document.addEventListener("click", function (e) { // One click handler for all f
     displayResult(buttonNum === questions[currentQuestion].correct);
     setTimeout(function () { // allow some time for the user to see the result
         currentQuestion++;
-        if (currentQuestion >= questions.length) return changePage();
+        if (currentQuestion >= questions.length) { 
+        // call function to store points in local storage, so we can display points on ending.html   
+        changePage();
+        }
         displayQuestion();
-    }, 1000); 
+    }, 1000);
+
 });
+}
 
 function displayQuestion() {
     question.textContent = questions[currentQuestion].question;
